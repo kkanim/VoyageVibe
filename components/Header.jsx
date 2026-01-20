85b12883e46af22da7f20027f5252598bed615c4
kkanim <cliffordanim100@gmail.com>
2026-01-20 22:47:01 +0000

    Fix Google Fonts link tags in index.html

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,9 @@
 
 export default function Header() {
   return (
-    <h1>Hello</h1>
+    <header className="header">
+      <img className="globe" src="/earth.png" alt="globe" />
+      <span>VoyageVibe</span>
+    </header>
   )
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import Header from "../components/Header";
-import "../src/App.css";
 
 export default function App() {
   return(
