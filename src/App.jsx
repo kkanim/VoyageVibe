51abd9509642d14cabef5637acaa16b1392bce45
kkanim <cliffordanim100@gmail.com>
2026-02-03 12:59:04 +0000

    Added .map

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import Header from "../components/Header";
 import Entry from "../components/Entry"
+import Data from "../src/data"
 
 export default function App() {
+  const entryElement = Data.map((data) => {
+    return <Entry 
+    img = {{
+      src: data.img.src,
+      alt: data.img.alt,
+    }}
+    title = {data.title}
+    country = {data.country}
+    googleMapLink = {data.googleMapsLink}
+    dates = {data.dates}
+    text = {data.text}
+    />
+  })
+  
   return(
     <>
       <Header />
-      <Entry />
+      <main className="container">
+        {entryElement}
+      </main>
     </>
   )
 }
---
+++ src/data.js
@@ -0,0 +1,38 @@
+export default [
+    {
+        id: 1,
+        img: {
+            src: "../images/Japan.png",
+            alt: "Mount Fuji"
+        },
+        title: "Mount Fuji",
+        country: "Japan",
+        googleMapsLink: "https://maps.app.goo.gl/6RLYZDuuuqJ7kNGZ9",
+        dates: "12 Jan, 2021 - 24 Jan, 2021",
+        text: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+    },
+    {
+        id: 2,
+        img: {
+            src: "../images/Independence.jpg",
+            alt: "Independence Square"
+        },
+        title: "Independence Square",
+        country: "Ghana",
+        googleMapsLink: "https://maps.app.goo.gl/Zr17SCrsJeCEKMd36",
+        dates: "27 May, 2021 - 8 Jun, 2021",
+        text: "Independence Square is a large public plaza in Accra, Ghana. Located in the Osu district near the coast, it is regarded as one of the most significant national monuments in Ghana and a symbol of the country's independence from colonial rule."
+    },
+    {
+        id: 3,
+        img: {
+            src: "../images/Big_hole.jpg",
+            alt: "Big Hole Pit Lake"
+        },
+        title: "Kimberley",
+        country: "South Africa",
+        googleMapsLink: "https://maps.app.goo.gl/fhkJuBhmFDv47tiB7",
+        dates: "01 Oct, 2021 - 18 Nov, 2021",
+        text: "The Big Hole, also known as Kimberley Mine, is a former diamond mine in Kimberley, South Africa. Located in the Northern Cape province, it is regarded as one of the largest hand-dug excavations in the world and a significant landmark in the history of diamond mining"
+    },
+]
